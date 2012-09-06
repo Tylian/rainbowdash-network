d9c10626a69a1c961988e1595be07a11e90ce7ea
Michael Shepard <joelisester@gmail.com>
2012-09-05 21:31:11 -0400

    Changed URL

--- plugins/CustomImage/CustomImagePlugin.php
+++ plugins/CustomImage/CustomImagePlugin.php
@@ -78,14 +78,17 @@ class CustomImagePlugin extends UAPPlugin
      */
     protected function showRectangle($action)
     {
-        
-	$action->elementStart('a', array('href' => 'http://www.chipin.com/contribute/id/742424f9788f1e7a'));
-		$action->element('img',
-                         array('width' => 180,
-                               'height' => 171,
-                               'src' => $this->path('donate.png')),
-                         '');
-$action->elementEnd('a');
+       $action->raw(<<<HERE
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHNwYJKoZIhvcNAQcEoIIHKDCCByQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYDAPz5kAYMktShOUvYW9W9aUOnr6N7+mBJl3Pm00wLGW1fBUIy+BzGMkHDeMvk3Oe2aAlmwqNwxMH35nbqp5nBXGIBTarqh2clyS/RI9gQY1Yitf8wsJiykUp+5RwJcRXcomNvJP+PdxOt1GnKhrCKoE4bgcgGqZgpuQdzXs4KprTELMAkGBSsOAwIaBQAwgbQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIxsjyBkdvxeuAgZAAsQiHVlHED+s2QWjwQ8J+/2mTit3qngKpsQwaniKsOdGsSxcu/DB5lCPMZRq/DGudTymjBvIqs0zY2OTaeApdCvIlN+YeizFJLdO1NSXsm2NF+Mv5kfVnQf+JHwhdbDPa2Uu5QAA8/DoGKTQUrzI4vqAhbWVhHfbhthCzbFs3nFuNIKGo5XycVUTCDZWPJrigggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xMjA5MDYwMTI2MDRaMCMGCSqGSIb3DQEJBDEWBBQZ9T8XHH3qsj61k8w/3F/Tduk3RTANBgkqhkiG9w0BAQEFAASBgC5U+aFix0jR73LCDr8Saa2fV6+o52phWBj0Of+7oVA60lSUjFTMXNEMqRj7iiCe+RYi0gE1qy8ajJCTUokyl6IcBjzdsXN3YwCLsqo2pMWkY6p5ZtfSv9iIP3Ct8uhVX4O80I45o/BQ16blD2+mP307OjaWKyzNWt46/pU1839R-----END PKCS7-----
+">
+<input type="image" src="http://rainbowdash.net/plugins/CustomImage/donate.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+HERE
+);
+
 }
 
     /**
