a33733a36608fc93dd29be3f93945a80d8a6346e
Michael Shepard <joelisester@gmail.com>
2012-09-29 03:41:14 -0400

    Fixed seize plugin so it won't break the profile pages.

--- plugins/Seize/SeizePlugin.php
+++ plugins/Seize/SeizePlugin.php
@@ -33,7 +33,7 @@ class SeizePlugin extends Plugin
     function onEndProfilePageActionsElements($action, $profile) {
         $cur = common_current_user();
 
-        if(($cur->hasRole(Profile_role::ADMINISTRATOR)
+        if(!empty($cur) && ($cur->hasRole(Profile_role::ADMINISTRATOR)
             //|| $cur->hasRole(Profile_role::MODERATOR)
         ) && $cur->id != $profile->id) {
             $sf = new SeizeForm($action, $profile,
