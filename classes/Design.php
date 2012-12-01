1498f45cf1578774affcb7a00f9ed70d5097fc26
Michael Shepard <joelisester@gmail.com>
2012-12-01 00:25:25 -0500

    Removed boxes on user profiles.

--- classes/Design.php
+++ classes/Design.php
@@ -53,8 +53,8 @@ class Design extends Memcached_DataObject
         }
         $ccolor  = Design::toWebColor($this->contentcolor);
         if (!empty($ccolor)) {
-            $css .= '#content, #site_nav_local_views li.current a, #site_nav_local_views li:hover a, ';
-			$css .= '#aside_primary .section { background-color: #';
+            $css .= '#content, #site_nav_local_views li.current a, #site_nav_local_views li:hover a';
+			$css .= '{ background-color: #';
             $css .= $ccolor->hexValue() . '} '."\n";
             $css .= '#site_notice, #anon_notice, #core { background-color: rgba(';
             $corecolor = hexdec($ccolor->hexValue());
