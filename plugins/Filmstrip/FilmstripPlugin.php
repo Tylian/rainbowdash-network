705b0ab0f127ab25b1f0775bba5b048a7b1afce3
Michael Shepard <joelisester@gmail.com>
2012-12-18 02:36:35 -0500

    Refactoring of Filmstrip to support user pages

--- plugins/Filmstrip/FilmstripPlugin.php
+++ plugins/Filmstrip/FilmstripPlugin.php
@@ -30,8 +30,9 @@ margin:0;
 }
 
 #core .filmstrip span.vcard .photo {
-margin-right:0;
 margin:0;
+margin-right:5px;
+margin-bottom:3px;
 padding:0;
 }
 
@@ -42,6 +43,62 @@ HERE
         );
     }
 
+    function outStrip($action, $attachment, $hideavatar=false) {
+
+        if(!empty($attachment)) {
+            $action->elementStart('div', array('class' => 'filmstrip thumbnails'));
+            //$totalwidth = 0;
+            while($attachment->fetch()) {
+
+                if($attachment->noticeCount()) {
+                    $at = new AttachmentListItem($attachment, $action);
+
+                    $notice = $attachment->stream();
+                    $notice->fetch();
+
+                    $thumb = $at->getThumbInfo();
+                    if ($thumb) {
+                        //$totalwidth += $thumb->width / $thumb->height * 63 + 6;
+                        //if ($totalwidth > 768) break;
+
+                        $action->elementStart('span', array('class' => 'inline-attachment'));
+                        $action->elementStart('a', $at->linkAttr());
+
+                        $action->element('img', array('alt' => '', 'src' => $thumb->url, 'width' => (int) ($thumb->width / $thumb->height * 63), 'height' => '63'));
+
+                        $action->elementEnd('a');
+
+                        if(!$hideavatar) $this->showProfile($notice, $action);
+                        $action->elementEnd('span');
+                    }
+                }
+            }
+            $action->elementEnd('div');
+
+            if(!empty($notice)) {
+                $notice->free();
+                unset($notice);
+            }
+        }
+    }
+
+    function onShowStreamNoticeList($notice, $action, $pnl) {
+        $notices = $action->user->getNotices(($action->page-1)*NOTICES_PER_PAGE, NOTICES_PER_PAGE + 1, null, null, true);
+
+        $attachments = array();
+        while($notices->fetch()) {
+            if($notices->hasAttachments()) {
+                $attachments = array_merge($attachments, $notices->attachments());
+            }
+        }
+
+        $attachments = new ArrayWrapper($attachments);
+
+        $this->outStrip($action, $attachments, true);
+
+        return true;
+    }
+
     function onEndShowAside($action) {
         if(get_class($action) == 'PublicAction') {
             $qry = "SELECT * FROM file WHERE modified > DATE_SUB(NOW(), INTERVAL 2 WEEK) ORDER BY RAND() LIMIT 20";
@@ -50,42 +107,7 @@ HERE
                 $qry,
                 30);
 
-            if(!empty($attachment)) {
-                $action->elementStart('div', array('class' => 'filmstrip thumbnails'));
-                //$totalwidth = 0;
-                while($attachment->fetch()) {
-
-                    if($attachment->noticeCount()) {
-                        $at = new AttachmentListItem($attachment, $action);
-
-                        $notice = $attachment->stream();
-                        $notice->fetch();
-
-                        $thumb = $at->getThumbInfo();
-                        if ($thumb) {
-                            //$totalwidth += $thumb->width / $thumb->height * 63 + 6;
-                            //if ($totalwidth > 768) break;
-
-                            $action->elementStart('span', array('class' => 'inline-attachment'));
-                            $action->elementStart('a', $at->linkAttr());
-
-                            $action->element('img', array('alt' => '', 'src' => $thumb->url, 'width' => (int) ($thumb->width / $thumb->height * 63), 'height' => '63'));
-
-                            $action->elementEnd('a');
-
-                            $this->showProfile($notice, $action);
-                            $action->elementEnd('span');
-                        }
-                    }
-                }
-                $action->elementEnd('div');
-
-                if(!empty($notice)) {
-                    $notice->free();
-                    unset($notice);
-                }
-            }
-
+            $this->outStrip($action, $attachment);
         }
 
         return true;
