e9e8a136db210df09e7fd15b70c768e8d190a762
Michael Shepard <joelisester@gmail.com>
2013-02-25 15:39:12 -0500

    Added custom prompt text setting.

--- lib/noticeform.php
+++ lib/noticeform.php
@@ -171,7 +171,7 @@ class NoticeForm extends Form
         if (Event::handle('StartShowNoticeFormData', array($this))) {
             $this->out->element('label', array('for' => 'notice_data-text',
                                                'id' => 'notice_data-text-label'),
-                                sprintf(_('What\'s up, %s?'), $this->user->nickname));
+            sprintf(common_config('site', 'prompttext') ? common_config('site', 'prompttext') : _('What\'s up, %s?'), $this->user->nickname));
             // XXX: vary by defined max size
             $this->out->element('textarea', array('id' => 'notice_data-text',
                                                   'cols' => 35,
