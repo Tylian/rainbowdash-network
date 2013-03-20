1ef53cf8104fd4b453149764041531ef7d4c1dba
Michael Shepard <joelisester@gmail.com>
2013-03-20 13:38:00 -0400

    Fixed duplicate items that can arise when scrolling with Meteor enabled.

--- plugins/InfiniteScroll/jquery.infinitescroll.js
+++ plugins/InfiniteScroll/jquery.infinitescroll.js
@@ -93,7 +93,7 @@
     
         if (props.isDuringAjax || props.isInvalidPage || props.isDone) return; 
     
-    		if ( opts.infiniteScroll && !isNearBottom(opts,props) ) return; 
+    		if ( opts.infiniteScroll && !isNearBottom(opts,props) ) return;
     		  
     		// we dont want to fire the ajax multiple times
     		props.isDuringAjax = true; 
@@ -113,7 +113,6 @@
     		box
     		  .attr('id','infscr-page-'+props.currPage)
     		  .addClass('infscr-pages')
-    		  .appendTo( opts.contentSelector )
     		  .load( path.join( props.currPage ) + ' ' + opts.itemSelector,null,function(){
     		    
     		        // if we've hit the last page...
@@ -137,9 +136,17 @@
       		            var scrollTo = $(window).scrollTop() + $('#infscr-loading').height() + opts.extraScrollPx + 'px';
                       $('html,body').animate({scrollTop: scrollTo}, 800,function(){ props.isDuringAjax = false; }); 
     		            }
+
+                    // check to make sure the posts don't already exist
+                    box.children().each(function() {
+                        if($('#' + $(this).attr('id')).length) {
+                            $(this).remove();
+                        }
+                    });
                     
                     // pass in the new DOM element as context for the callback
                     callback.call( box[0] );
+                    box.appendTo( opts.contentSelector );
 
                     // pushState (if supported)
                     if (typeof history.pushState !== 'undefined') {
@@ -151,7 +158,7 @@
 
     		            if (!opts.animate) props.isDuringAjax = false; // once the call is done, we can allow it again.
   	            }
-    		    }); // end of load()
+              }); // end of load()
     			
     		    
       }  // end of infscrSetup()
